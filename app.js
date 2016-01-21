018aaed1569fed16b1732cca2a03b06af2901a2b
Green <green7512@gmail.com>
2016-01-21 19:01:41 +0100

    box

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  $('.suggestions').hide();
+
+    setTimeout(function() {   //calls click event after a certain time
+       $('.suggestions').fadeIn();
+    }, 2000);
+
+
 
   $('.scroll').click(function(){
     var page = $(this).attr('href');
